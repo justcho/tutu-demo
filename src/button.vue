99cfbd7f32659eda910369c31dc70998c1cded7e
justchoe <1620845677@qq.com>
2020-08-13 18:08:21 +0800

    验证 iconPosition 为 left 或 right 中的一个

--- src/button.vue
+++ src/button.vue
@@ -10,25 +10,63 @@
 </template>
 <script>
   export default {
-    props: ['icon', 'iconPosition']
+    // props: ['icon', 'iconPosition']
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return value === `left` || value === 'right';
+        }
+      }
+    }
   }
 </script>
 <style lang="scss">
     .g-button {
-        font-size: var(--font-size);height: var(--button-height);padding: 1em;
-        border-radius: var(--bordor-radius);border: 1px solid var(--border-color);
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 1em;
+        border-radius: var(--bordor-radius);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-        display: inline-flex;justify-content: center;align-items: center;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         vertical-align: middle;
-        &:hover {border-color: var(--border-color-hover);}
-        &:active {background-color: var(--button-active-bg);}
-        &:focus {outline: none;}
-        > .content {order: 2;}
-        > .icon {order: 1; margin-right: .1em;}
+
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
 
         &.icon-right {
-            > .content {order: 1;}
-            > .icon {order: 2; margin-right: 0; margin-left: .1em;}
+            > .content {
+                order: 1;
+            }
+
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+            }
         }
     }
 

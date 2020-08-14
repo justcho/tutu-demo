e77e131a6ba4ec8dbd20c629e6f23bd73b0fe370
justchoe <1620845677@qq.com>
2020-08-14 12:14:27 +0800

    创建 button  group

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
-    @click="$emit('click')">
+            @click="$emit('click')">
         <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
         <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
@@ -13,7 +13,7 @@
     // props: ['icon', 'iconPosition']
     props: {
       icon: {},
-      loading:{
+      loading: {
         type: Boolean,
         default: false
       },
@@ -29,59 +29,31 @@
 </script>
 <style lang="scss">
     @keyframes spin {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
+        0% {transform: rotate(0deg);}
+        100% {transform: rotate(360deg);}
     }
 
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 1em;
-        border-radius: var(--bordor-radius);
-        border: 1px solid var(--border-color);
+    .g-button {font-size: var(--font-size);height: var(--button-height); padding: 0 1em;
+        border-radius: var(--bordor-radius); border: 1px solid var(--border-color);
         background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        vertical-align: middle;
+        display: inline-flex;justify-content: center;align-items: center;vertical-align: middle;
 
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
+        &:hover {border-color: var(--border-color-hover);}
 
-        &:active {
-            background-color: var(--button-active-bg);
-        }
+        &:active {background-color: var(--button-active-bg);}
 
-        &:focus {
-            outline: none;
-        }
+        &:focus {outline: none;}
 
-        > .content {
-            order: 2;
-        }
+        > .content {order: 2;}
 
-        > .icon {
-            order: 1;
-            margin-right: .1em;
-        }
+        > .icon {order: 1;margin-right: .1em;}
 
         &.icon-right {
-            > .content {
-                order: 1;
-            }
+            > .content {order: 1;}
 
-            > .icon {
-                order: 2;
-                margin-right: 0;
-                margin-left: .1em;
-            }
+            > .icon {order: 2; margin-right: 0;margin-left: .1em;}
         }
-        .loading{
+        .loading {
             animation: spin 1s infinite linear;
         }
     }

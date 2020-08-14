13eb6ee6fc3aaea790e474659df171106fbacd1a
justchoe <1620845677@qq.com>
2020-08-14 09:16:09 +0800

    将 svg 代码整理到   icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <svg v-if="icon" class="icon">
-            <use :xlink:href="`#i-${icon}`"></use>
-        </svg>
+        <g-icon  v-if="icon" :name="icon"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -18,7 +16,7 @@
         default: 'left',
         validator(value) {
           return value === `left` || value === 'right';
-        }
+      }
       }
     }
   }

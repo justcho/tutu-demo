ef7ee55a4e70acb1c982ea81f8392eae809c3ade
justchoe <1620845677@qq.com>
2020-08-18 00:29:12 +0800

    添加 input 的测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input :value="value" type="text" :disabled="disabled" :readonly="readonly">
+        <input :value="value" type="text" :disabled="disabled" :readonly="readonly"
+               @change="$emit('change',$event)"
+               @input="$emit('input',$event)"
+               @focus="$emit('focus',$event)"
+               @blur="$emit('blur',$event)"
+        >
         <template v-if="error">
-            <icon  name="error" class="icon-error"></icon>
+            <icon name="error" class="icon-error"></icon>
             <span class="errorMessage">{{error}}</span>
         </template>
     </div>
@@ -41,16 +46,24 @@
     $red: #F1453D;
     .wrapper { font-size: $font-size; display: inline-flex;
         align-items: center;
+
         > :not(:last-child) {margin-right: .5em; }
+
         > input { height: 32px; border: 1px solid $border-color; border-radius: 4px; padding: 0 8px; font-size: inherit;
+
             &:hover { border-color: $border-color-hover; }
+
             &:focus { box-shadow: inset 0 1px 3px $box-shadow-color; outline: none; }
+
             &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed; }
         }
+
         &.error {
             > input { border-color: $red; }
         }
+
         .icon-error { fill: $red; }
+
         .errorMessage { color: $red; }
     }
 

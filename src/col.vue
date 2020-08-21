9cc7c3083ac682bd6268eed31f572b1ecc5a2700
justchoe <1620845677@qq.com>
2020-08-21 10:00:20 +0800

    稍微重构了一下

--- src/col.vue
+++ src/col.vue
@@ -45,13 +45,13 @@
     computed: {
       colClass () {
         let {span, offset, ipad, narrowPc, pc, widePc} = this
+        let createClasses = this.createClasses
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          ... (ipad ? [`col-ipad-${ipad.span}`] : []),
-          ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-          ... (pc ? [`col-pc-${pc.span}`] : []),
-          ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+          ...createClasses({span, offset}),
+          ...createClasses(ipad, 'ipad-'),
+          ...createClasses(narrowPc, 'narrow-pc-'),
+          ...createClasses(pc, 'pc-'),
+          ...createClasses(widePc, 'wide-pc-'),
         ]
       },
       colStyle () {

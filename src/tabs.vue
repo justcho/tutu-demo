efb435682695d8340dbc0ea003112bd6faee7299
justchoe <1620845677@qq.com>
2020-09-01 11:06:01 +0800

    重构 tabs

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,22 +31,29 @@
         eventBus: this.eventBus
       }
     },
-    mounted() {
-      if(this.$children.length ===0){
-        console && console.warn &&
-        console.warn('tabs的子组件应该是tabs-head 和 tabs-nav,但你没有些子组件')
-      }
-      this.$children.forEach((vm) => {
-        if(vm.$options.name === 'TutuTabsHead'){
-          vm.$children.forEach((childVm)=>{
-            if(childVm.$options.name= 'TutuTabsItem'
-              && childVm.name === this.selected){
-              this.eventBus.$emit('update:selected', this.selected,childVm)
-            }
-          })
+    methods: {
+      checkChildren() {
+        if (this.$children.length === 0) {
+          console && console.warn &&
+          console.warn('tabs的子组件应该是tabs-head 和 tabs-nav,但你没有些子组件')
         }
-      })
-
+      },
+      selectTab(){
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === 'TutuTabsHead') {
+            vm.$children.forEach((childVm) => {
+              if (childVm.$options.name = 'TutuTabsItem'
+                && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted() {
+      this.checkChildren();
+      this.selectTab();
     }
   }
 </script>

57a149223617ccd3df3e914d1169cfdab7a99f1b
justchoe <1620845677@qq.com>
2020-09-02 18:43:29 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collapseItem">
-        <div class="title" @click="open=!open">
+        <div class="title" @click="toggle">
             {{title}}
         </div>
         <div class="content" v-if="open">
@@ -18,9 +18,30 @@
         required: true
       }
     },
-    data () {
+    data() {
       return {
         open: false
+      }
+    },
+    inject: ['eventBus'],
+    mounted() {
+      this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
+        if(vm!==this){
+          this.close()
+        }
+      })
+    },
+    methods:{
+      toggle() {
+        if (this.open) {
+          this.open = false
+        } else {
+          this.open = true
+          this.eventBus.$emit && this.eventBus.$emit('update:selected',this)
+        }
+      },
+      close(){
+        this.open = false
       }
     }
   }
@@ -40,18 +61,21 @@
             align-items: center;
             padding: 0 8px;
         }
+
         &:first-child {
             > .title {
                 border-top-left-radius: $border-radius;
                 border-top-right-radius: $border-radius;
             }
         }
+
         &:last-child {
             > .title:last-child {
                 border-bottom-left-radius: $border-radius;
                 border-bottom-right-radius: $border-radius;
             }
         }
+
         > .content {
             padding: 8px;
         }
